6a7046230e2776ed043939401aa22bae31284ecc
alumno1 <alumno1@gmail.com>
2017-11-16 12:45:08 +0100

    articulos 3

--- app/controllers/articulos_controller.rb
+++ app/controllers/articulos_controller.rb
@@ -1,11 +1,18 @@
 class ArticulosController < ApplicationController
   def new
+    @articulo= Articulo.new
   end
 
   def index
   end
 
   def create
+      @articluo= Articulo.new(articulo_params)
+      if @articulo.save
+        redirect_to @articulo
+      else
+        render 'new'
+      end
   end
 
   def edit
@@ -18,5 +25,10 @@ class ArticulosController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  def articulo_params
+    params.require(:articulo).permit(:titulo, :contenido)
   end
 end
--- app/models/articulo.rb
+++ app/models/articulo.rb
@@ -1,3 +1,7 @@
 class Articulo < ApplicationRecord
   belongs_to :autor
+
+  validates :titulo, presence: {:message => "no vacio"}
+  validates :contenido, presence: {:message => "no vacio"}
+  lenghth: {minimum: 50, message => "minimo 50"}
 end
